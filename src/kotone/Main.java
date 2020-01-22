14b66c1aa977527cc48f745c8197905519b00638
kotonetamura <t.koto14ne0217@gmail.com>
2020-01-22 11:59:54 +0900

    kotone commit

--- src/kotone/Main.java
+++ src/kotone/Main.java
@@ -5,17 +5,32 @@ import processing.core.PApplet;
 public class Main extends PApplet {
     @Override
     public void settings(){
-        size(1280, 720);
+        size(600, 400);
     }
 
     @Override
-    public void setup(){
+    public void setup() {
 
+        background(255);
     }
+
+    float x =300;
+    float y =200;
+    int r = 180;
+
+
 
     @Override
     public void draw(){
-
+        //background(255);
+        fill(255,10);
+        noStroke();
+        rect(0,0,600,400);
+        x = x + 1;
+        //y = y + random(-4,4);
+        noFill();
+        stroke(0,0,255);
+        rect(x,y,r,r);
     }
 
     public static void main(String[] args){
