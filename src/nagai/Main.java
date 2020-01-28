24e5d91c9c8d206aaa0af13c87c9474cc1504778
HirrokiLucky <nagadon0424@icloud.com>
2020-01-28 10:06:35 +0900

    nagai commit

--- src/nagai/Main.java
+++ src/nagai/Main.java
@@ -3,18 +3,74 @@ package nagai;
 import processing.core.PApplet;
 
 public class Main extends PApplet {
+    boolean right = false;
+    boolean left = false;
+    boolean up = false;
+    boolean down = false;
+    float x = 0;
+    float y = 0;
+    double xspeed = 3.0;
+    double yspeed = 3.0;
+
+
     @Override
     public void settings(){
-
+        size(1000,500);
     }
 
     @Override
     public void setup(){
 
+
     }
+    @Override
+    public void keyPressed(){
+        if (keyCode == RIGHT) right = true;
+        if (keyCode == LEFT) left = true;
+        if (keyCode == UP) up = true;
+        if (keyCode == DOWN) down = true;
+    }
+    @Override
+    public void keyReleased() {
+        if (keyCode == RIGHT) right = false;
+        if (keyCode == LEFT) left = false;
+        if (keyCode == UP) up = false;
+        if (keyCode == DOWN) down = false;
+    }
+
 
     @Override
     public void draw(){
+        background(HSB,50,0,50);
+
+        if (right){
+            x += xspeed;
+
+        }
+        if (left){
+            x -= xspeed;
+        }
+        if (up){
+            y -= yspeed;
+        }
+        if (down){
+            y += yspeed;
+        }
+        if(y>height-30){
+            yspeed = -yspeed;
+        }
+        if(y<0) {
+            yspeed = -yspeed;
+        }
+        if(x<0){
+            xspeed=-xspeed;
+        }
+        if(x>width-50){
+            xspeed=-xspeed;
+        }
+        noStroke();
+        rect(x,y,50,30);
+
 
     }
 
---
+++ src/nagai/Zikken2.java
@@ -0,0 +1,131 @@
+package nagai;
+
+import processing.core.PApplet;
+
+public class Zikken2 extends PApplet  {
+    int gamemode = 0;
+    boolean W = false;
+    boolean D = false;
+    boolean S = false;
+    boolean A = false;
+    boolean Dash = false;  //shiftを定義するか、Wで定義するか
+    float x = 450;
+    float y = 270;
+    double xspeed = 3.0;
+    double yspeed = 3.0;
+    double DashSpeed = 6.0;
+    int Life = 1;
+
+    @Override
+    public void settings(){
+        size(1000,600);
+    }
+    @Override
+    public void setup(){
+    }
+
+    @Override
+    public void keyPressed(){
+        if (key == 'w') W = true;   //キーボードのときはkeyCodeはダメ
+        if (key == 'a') A = true;
+        if (key == 's') S = true;
+        if (key == 'd') D = true;
+        if(keyCode == SHIFT) Dash = true;
+    }
+    @Override
+    public void keyReleased(){
+        if (key == 'w') W = false;
+        if (key == 'a') A = false;
+        if (key == 's') S = false;
+        if (key == 'd') D = false;
+        if(keyCode == SHIFT) Dash = false;
+    }
+    public void draw(){
+        background(0);
+        if(gamemode == 0){
+            title();
+        }else if(gamemode == 1){
+            PlayerMove();
+            PlayerDisp();
+            EnemyDisp();
+            EnemyMove();
+            LifeCount();
+            if(Life == 0){
+                gamemode = 2;
+            }
+        }else if(gamemode == 2){
+            GameOver();
+        }
+    }
+    void title(){
+        textSize(100);
+        fill(100,200,150);
+        textAlign(CENTER);
+        text("Press Key",500,300);
+
+        if(keyPressed==true){
+            gamemode=1;
+        }
+    }
+    void PlayerMove(){
+        if(D && Dash){
+            x += DashSpeed;
+        }
+        if(A && Dash){
+            x -= DashSpeed;
+        }
+        if(W && Dash){
+            y -= DashSpeed;
+        }
+        if(S && Dash){
+            y += DashSpeed;
+        }
+        if (D){
+            x += xspeed;
+        }
+        if (A){
+            x -= xspeed;
+        }
+        if (W){
+            y -= yspeed;
+        }
+        if (S){
+            y += yspeed;
+        }
+        if(y>height-30){
+            y=570;
+        }
+        if(y<0) {
+            y=0;
+        }
+        if(x<0){
+            x=0;
+        }
+        if(x>width-50){
+            x=950;
+        }
+    }
+    void PlayerDisp(){
+        noStroke();
+        rect(x,y,50,30);
+    }
+    void EnemyDisp(){
+        rect(900,300,30,15);
+    }
+    void EnemyMove(){
+    }
+    void LifeCount(){
+        if((900==(x+50) || 930==x )&&(300<=(y+30) && 315>=y)){
+            Life = Life - 1;
+        }
+
+    }
+    void GameOver(){
+        text("Game Over",500,200);
+        textAlign(CENTER);
+        textSize(100);
+    }
+    public static void main(String[] args){
+        PApplet.main("nagai.Zikken2");
+    }
+}
