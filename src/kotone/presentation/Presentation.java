0ac8f74f7fdbec5737240e2eeb2f79697ce627af
kotonetamura <t.koto14ne0217@gmail.com>
2020-02-04 23:31:29 +0900

    kotone commit

--- src/kotone/presentation/Presentation.java
+++ src/kotone/presentation/Presentation.java
@@ -2,6 +2,7 @@ package kotone.presentation;
 
 import processing.core.PApplet;
 import processing.core.PImage;
+import kotone.presentation.Main;
 
 public class Presentation extends PApplet {
 
@@ -11,6 +12,10 @@ public class Presentation extends PApplet {
     float pointX;
     float pointY;
     float speedY;
+    Main combat;//タイトルクラスのオブジェクトを作る
+    int m_hp = 20;
+    int before = m_hp;
+
 
     @Override
     public void settings(){
@@ -30,16 +35,20 @@ public class Presentation extends PApplet {
         speedY = 0.5f;
 
         efect = loadImage("C:\\Users\\tkoto\\Downloads\\kaenbeameffect\\火炎ビームエフェクトアニメ\\m\\kaenbeam.png");
+
+        combat = new Main();//タイトルクラスのオブジェクトにタイトルクラスをインスタンス化していれる
     }
 
     @Override
     public void draw(){
 
+        combat.draw();//タイトルクラスの中身が使えるようにする
+
         image(haikei,width/2,height/2);//背景の大きさ
         image(monster,pointX,pointY);
         monsterMove();
 
-        if(m_hp > new m_hp) {
+        if(m_hp < before) {
             tint(255, 80, 31);//敵が攻撃を受けたときに赤い色を付ける
             image(monster, pointX, pointY);
         }
--- src/kotone/presentation/Presenttion2.java
+++
@@ -1,8 +0,0 @@
-package kotone.presentation;
-
-public class Presenttion2 {
-
-    public static void main(String[] args){
-        Presentation presentation1 = new Presentation
-    }
-}
