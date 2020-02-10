b361ccd652843f79552331353af8875c9f7391ab
kotonetamura <t.koto14ne0217@gmail.com>
2020-02-10 12:48:16 +0900

    kotone commit

--- src/kotone/presentation/Presentation.java
+++ src/kotone/presentation/Presentation.java
@@ -2,8 +2,11 @@ package kotone.presentation;
 
 import processing.core.PApplet;
 import processing.core.PImage;
+import hibino.Main;
 
 import java.util.concurrent.atomic.AtomicInteger;
+
+import static hibino.Main.*;
 
 //import static kotone.presentation.Main.m_hp;
 //import static kotone.presentation.Main.p_hp;
@@ -17,10 +20,9 @@ public class Presentation {
     float pointX;
     float pointY;
     float speedY;
-    float nowP_hp;
-    float nowM_hp;
-    int p_lifegaugeWidth;
-    int m_lifegaugeWidth;
+    float m_drawWidth;
+    float p_rectWidth = 200;
+    float m_rectWidth = 200;
     float alpha = 255f;       //透明度
     boolean cngAlpha;    //変化開始FLG
     public static boolean fadeMode;    //フェードイン・アウト切り替えFLG
@@ -44,12 +46,6 @@ public class Presentation {
         pointX = pApplet.width/2;
         pointY = 300;
         speedY = 1;
-
-//        nowP_hp = p_hp; //現在の残りHP
-//        nowM_hp = Main.m_hp; //現在の残りHP
-//        //lifegauge = pApplet.loadImage("C:\\Users\\tkoto\\Downloads\\nc81777.jpgr");
-//        lifegauge.resize(280,10);
-//        p_lifegaugeWidth = 280;
 
         efect = pApplet.loadImage("C:\\Users\\tkoto\\Downloads\\kaenbeameffect\\火炎ビームエフェクトアニメ\\m\\kaenbeam.png");
 
@@ -82,6 +78,47 @@ public class Presentation {
         if (!fadeMode) {
             fadeOut();
         }
+
+        if (p_hp < 25) {
+            System.out.println("1/4");
+            pApplet.fill(255,0,0);
+        } else if(p_hp < 50){
+            System.out.println("1/2");
+            pApplet.fill(255,200,0);
+        } else {
+            System.out.println("p");
+            pApplet.fill(0,255,0);
+        }
+
+        pApplet.noStroke();
+        float p_drawWidth = (p_hp / p_hp_max)*p_rectWidth;
+        pApplet.rect(100,100,p_drawWidth,50);
+
+        pApplet.stroke(0);
+        pApplet.noFill();
+        pApplet.rect(100,100,p_rectWidth,50);
+
+        if (m_hp < 5) {
+            System.out.println("m1/4");
+            pApplet.fill(255,0,0);
+        } else if(m_hp < 10){
+            System.out.println("m1/2");
+            pApplet.fill(255,200,0);
+        } else {
+            System.out.println("m");
+            pApplet.fill(0,255,0);
+        }
+
+        pApplet.noStroke();
+        if (hibino.Main.m_hit = true) {
+            m_drawWidth = (m_hp / m_hp_max) * m_rectWidth;
+            hibino.Main.m_hit = false;
+        }
+            System.out.println("rect_m");
+        pApplet.rect(1000,100,m_drawWidth,50);
+        pApplet.stroke(0);
+        pApplet.noFill();
+        pApplet.rect(1000,100,m_rectWidth,50);
     }
 
     public void monsterMove(){//普通のモンスターのスピード
@@ -95,7 +132,7 @@ public class Presentation {
             speedY = -1;
         }
 
-        if(Main.m_hp < 10){
+        if(m_hp < 10){
             if(pointY < 290){
                 speedY = 0.5f;
             }
