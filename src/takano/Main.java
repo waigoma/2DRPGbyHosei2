408ccce8e97ae40bc98053bd40fb447b1c106a0e
noir- <syougi.jun1-2628@docomo.ne.jp>
2020-02-13 11:16:57 +0900

    takano commit

--- src/takano/Main.java
+++ src/takano/Main.java
@@ -1,24 +1,58 @@
 package takano;
 
-import processing.core.PApplet;
+import  processing.core.PApplet;
 
-public class Main extends PApplet {
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+public class Main extends PApplet{
     @Override
     public void settings(){
-
+        size(480,480);
     }
-
     @Override
-    public void setup(){
+    public void  setup(){
 
     }
-
     @Override
     public void draw(){
 
     }
-
     public static void main(String[] args){
-        PApplet.main("takano.Main");
+        Clip clip = createClip(new File("src/takano/bgm/キラー・シルエット"));
+        clip.start();
+    }
+
+    public static Clip createClip(File path) {
+        try (AudioInputStream ais = AudioSystem.getAudioInputStream(path)){
+
+            AudioFormat af = ais.getFormat();
+
+            DataLine.Info dataLine = new DataLine.Info(Clip.class,af);
+
+            Clip c = (Clip)AudioSystem.getLine(dataLine);
+            
+            c.open(ais);
+
+            return c;
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (UnsupportedAudioFileException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (LineUnavailableException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
---
+++ src/takano/Takano1.java
@@ -0,0 +1,62 @@
+package takano;
+
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import javax.sound.sampled.Clip;
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+
+
+
+public class Takano1 {
+    public static void main(String[] arg) throws Exception {
+        String bgm1 = "src/takano/bgm/キラーズ・シルエット.wav";//ボス戦BGM
+        String bgm2 = "src/takano/bgm/彩づく草原.wav";//村BGM
+        String bgm3 = "src/takano/bgm/森のいざない.wav";//ダンジョンBGM
+        String bgm4 = "src/takano/bgm/Battle_Thema.wav";//敵との闘いBGM
+        String bgm5 = "src/takano/bgm/なんとか大作戦.wav";//エンディングBGM
+        String bgm6 = "src/takano/bgm/8bit-act01_title.wav";//タイトル画面BGM
+
+        String effect1 = "src/takano/bgm/斬撃音_1.wav";//攻撃当たる効果音
+        String effect2 ="src/takano/bgm/始まりを告げる喇叭.wav";//敵に勝った時効果音
+        String effect3 ="src/takano/bgm/システムSE_選択音1.wav";
+        String effect4 ="src/takano/bgm/serif3.wav";//表示
+        String effect5 ="src/takano/bgm/kettei-01.wav";//タイトル選択
+        String effect6 ="src/takano/bgm/tousou-o1.wav";//逃げる
+
+        Clip clip = creatClip(new File(bgm6));
+        clip.start();//読み込み
+        clip.loop(Clip.LOOP_CONTINUOUSLY);//無限ループ
+        Thread.sleep(1000*10);//流れる時間
+        clip.close();//読み込み終わり
+    }
+
+    public static Clip creatClip(File path) {           //バグが起きたときの対処
+        try (AudioInputStream ais = AudioSystem.getAudioInputStream(path)){
+
+            AudioFormat af = ais.getFormat();
+
+            DataLine.Info dataLine = new DataLine.Info(Clip.class,af);
+
+            Clip c = (Clip)AudioSystem.getLine(dataLine);
+
+            c.open(ais);
+
+            return c;
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (UnsupportedAudioFileException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (LineUnavailableException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
